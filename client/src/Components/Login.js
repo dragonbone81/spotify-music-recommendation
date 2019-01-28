c979f2f15a7145b08c487f1c0ba83b6fd6d37189
Christian <cvernikoff@gmail.com>
2019-01-28 11:39:54 -0800

    redid login request a little

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -14,7 +14,7 @@ class Login extends Component {
         // );
         // popup.addEventListener("unload", doStuffOnUnload, false);
         // window.open('http://localhost:3000/callback')
-        window.location = `https://accounts.spotify.com/authorize?client_id=${'53426d0fc31e498aabbdc9cd01e1a122'}&redirect_uri=http://localhost:3001&response_type=code`
+        window.location = `https://accounts.spotify.com/authorize?client_id=${'e2cdf918537c45a687015a05f70949a8'}&redirect_uri=http://localhost:3001&response_type=code`
     };
 
     render() {
--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-const axios = require("axios");
-const qs = require("querystring");
+let request = require('request');
 // const client = require("./db_connection");
 // const queries = require("./db_queries");
 // const checkJWT = require("./middleware");
@@ -14,20 +13,23 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get("/", async (req, res) => {
-    const spotify = await axios.post('https://accounts.spotify.com/api/token', qs.stringify({
-        grant_type: "authorization_code",
-        code: req.query.code,
-        redirect_uri: 'http://localhost:3001',
-        client_id: 'xx',
-        client_secret: 'xx'
-    }), {
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/x-www-form-urlencoded'
+    request.post({
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+            code: req.query.code,
+            redirect_uri: 'http://localhost:3001',
+            grant_type: 'authorization_code',
+            client_id: 'xx',
+            client_secret: 'xx',
+        },
+        json: true
+    }, (error, response, body) => {
+        if (error) {
+            res.redirect(process.env.FRONTEND_URI || 'http://localhost:3000' + '/fuckit');
+        } else {
+            res.redirect(process.env.FRONTEND_URI || 'http://localhost:3000' + '/callback?access_token=' + body.access_token)
         }
     });
-    console.log(spotify.data);
-    res.redirect(`http://localhost:3000/callback?access_token=${spotify.data.access_token}`);
 });
 
 const server = app.listen(

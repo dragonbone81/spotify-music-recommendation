113d1698b6fc58128b622df989177461753efa29
Christian <cvernikoff@gmail.com>
2019-01-30 20:37:43 -0800

    removed the spaces

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -5,8 +5,7 @@ import api from '../api/api';
 class Login extends Component {
     SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
     spotify = () => {
-        window.location = `https://accounts.spotify.com/authorize?client_id=${'e2cdf918537c45a687015a05f70949a8'}
-        &redirect_uri=${this.SERVER_URL}/login&response_type=code&scope=user-follow-read%20user-read-private`
+        window.location = `https://accounts.spotify.com/authorize?client_id=${'e2cdf918537c45a687015a05f70949a8'}&redirect_uri=${this.SERVER_URL}/login&response_type=code&scope=user-follow-read%20user-read-private`
     };
 
     render() {

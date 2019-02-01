c0d5fe8eba1bec1ac7e7c90fc45fd3d72a7cd4b1
Christian <cvernikoff@gmail.com>
2019-01-31 18:06:47 -0800

    fixed the arrow

--- client/src/Components/Dashboard.js
+++ client/src/Components/Dashboard.js
@@ -216,7 +216,11 @@ class Dashboard extends Component {
                              className={`expand-content ${!this.state.options ? "hidden" : ""}`}>
                             <div onClick={() => this.setState({options: !this.state.options})} className="pull-tab">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-                                    <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
+                                    {this.state.options ?
+                                        <path d="M0 16.67l2.829 2.83 9.175-9.339 9.167 9.339 2.829-2.83-11.996-12.17z"/>
+                                        :
+                                        <path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z"/>
+                                    }
                                 </svg>
                             </div>
                             <div onClick={(e) => e.stopPropagation()} className="options-content">
